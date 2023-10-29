4a3aea3c324bcb1a5234065b7820b3fbefb049a3
abi-aleph <abi.farhan@aleph-labs.com>
2023-10-29 21:42:33 +0700

    Create `CryptoFeed` model

---
+++ app/src/main/java/com/ourproject/cryptotddcase/CryptoFeed.kt
@@ -0,0 +1,21 @@
+package com.ourproject.cryptotddcase
+
+data class CryptoFeed(
+    val coinInfo: CoinInfo,
+    val raw: Raw
+)
+
+data class CoinInfo(
+    val id: String,
+    val name: String,
+    val fullName: String
+)
+
+data class Raw(
+    val usd: Usd
+)
+
+data class Usd(
+    val price: Double,
+    val changePctDay: Long
+)
