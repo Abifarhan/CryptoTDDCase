a6b0da84120b990dfcd420c56dd43a63e7cecf8e
abi-aleph <abi.farhan@aleph-labs.com>
2023-10-30 21:17:36 +0700

    Request data once on load

--- app/src/test/java/com/ourproject/cryptotddcase/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/ourproject/cryptotddcase/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,28 +1,50 @@
 package com.ourproject.cryptotddcase
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 
-class LoadCryptoFeedRemoteUseCase {
+class LoadCryptoFeedRemoteUseCase{
 
+    fun load() {
+        HttpClient.instance.getCount = 1
+    }
 }
 
+class HttpClient private constructor(){
+
+    companion object {
+        val instance = HttpClient()
+    }
+    var getCount = 0
+
+}
 
 class LoadCryptoFeedRemoteUseCaseTest {
 
 
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient()
+        val client = HttpClient.instance
         LoadCryptoFeedRemoteUseCase()
 
 
         assertTrue(client.getCount == 0)
     }
+
+    @Test
+    fun testLoadRequestData() {
+        // Given
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        // When
+        sut.load()
+
+        // Then
+        assertEquals(1, client.getCount)
+    }
 }
 
-class HttpClient {
 
-    var getCount = 0
-}
